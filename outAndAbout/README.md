a7a35a3494865f9a855f3cd2c15e3f283824850a
David Lago <davidlago@gmail.com>
2014-08-15 19:10:29 -0400

    Added out and about script and resulting csv

--- outAndAbout/README.md
+++ outAndAbout/README.md
@@ -1,2 +1,14 @@
 # Out and about
 The purpose of this script is producing a dataset containing the number of transactions per zipcode per 15-minute interval. We can use this data as a proxy for how many people were in a specific zipcode area at a given time. We will aggregate data from T station transactions and cab pick-ups and drop-offs. For simplicity, we will equate one cab transaction to one person, although we might introduce weights if we need to (maybe 1.5 people in average per ride).
+
+There are four 15-minute windows: `00`, `15`, `30` and `45`. Since we are using the number of transactions as a proxy for the number of people at a speficic time, the following adjustments were made to the datasets:
+
+* Train ridership: since these data are already divided in 15-minute blocks, their dates are unchanged.
+* Bus ridership: no data is used (missing zipcodes or location of bus stops).
+* Cab pick-ups and drop-offs: we will round them to the nearest 15-minunte interval.
+
+####Field descriptions:
+
+* `zip`: 5 digit string representation of the zipcode.
+* `datetime`: date and time of the start of the 15-minute window. Format: `yyyy-mm-dd HH:MM:ss`.
+* `tx`: number of transactions.
--- outAndAbout/getTransactionsByZip.R
+++ outAndAbout/getTransactionsByZip.R
@@ -1,2 +1,46 @@
 # Loads datasets
 load('../import/lateNightT.R');
+
+library(plyr);
+
+print('Loading data...');
+txCabStart <- data.frame(cab$startZip, cab$startDate, 1);
+names(txCabStart) <- c('zip','datetime','tx');
+
+print('Rounding dates...');
+# round DOWN dates
+txCabStart$datetime <- as.POSIXct(format(strptime("1969-12-31 19:00", "%Y-%m-%d %H:%M") +
+        round(as.numeric(txCabStart$datetime)/900)*900,"%Y-%m-%d %H:%M:%S"));
+
+txCabEnd <- data.frame(cab$endZip, cab$endDate, 1);
+names(txCabEnd) <- c('zip','datetime','tx');
+# round UP dates
+txCabEnd$datetime <- as.POSIXct(format(strptime("1969-12-31 19:00", "%Y-%m-%d %H:%M") +
+        round(as.numeric(txCabEnd$datetime)/900)*900,"%Y-%m-%d %H:%M:%S"));
+
+# group train transactions by zipcode and date
+print('Grouping trains...');
+txTrain <- ddply(ridership[!is.na(ridership$zip),], c('zip','datetime'), function(df)sum(df$tx));
+names(txTrain) <- c('zip','datetime','tx');
+
+# join datasets
+txs <- rbind(txTrain,txCabStart,txCabEnd);
+names(txs) <- c('zip','datetime','tx');
+
+# group again, now with all the data
+print('Final grouping...');
+txs <- ddply(txs, c('zip','datetime'), function(df)sum(df$tx));
+names(txs) <- c('zip','datetime','tx');
+
+# Delete unneeded data frames
+rm(txCabStart,txCabEnd,txTrain);
+
+# Write out CSV file
+print('Writing CSV file...');
+write.csv(txs,'outandabout.csv',row.names=FALSE);
+
+# Save R objects
+print('Saving R object...');
+save(txs,file='outAndAbout.R');
+
+print('Done!');
