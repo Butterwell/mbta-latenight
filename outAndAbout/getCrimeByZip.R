ec80f80ad2822830c19c0ab093038d2c9ec87393
David Lago <davidlago@gmail.com>
2014-08-18 16:13:39 -0400

    Filtered crime data by weekend only entires

--- outAndAbout/getCrimeByZip.R
+++ outAndAbout/getCrimeByZip.R
@@ -4,6 +4,7 @@
 ################################################################
 
 # Load dataset
+print('Importing data...');
 unzip('../data/crime.zip', exdir='../data')
 crime <- read.csv('../data/Crime_Incident_Reports.csv', header=TRUE);
 
@@ -12,8 +13,11 @@ crime$FROMDATE <- as.POSIXct(strptime(crime$FROMDATE,"%m/%d/%Y %I:%M:%S %p"));
 # Filter out entries prior to 2013-03-01
 crime <- crime[crime$FROMDATE >= as.POSIXct(strptime("2013-03-01", "%Y-%m-%d")),];
 crime <- crime[!is.na(crime$COMPNOS),];
+# Limit to just weekend
+crime <- crime[crime$DAY_WEEK == 'Friday' | crime$DAY_WEEK == 'Saturday' | crime$DAY_WEEK == 'Sunday',];
 
 # Split longitude and latitude
+print('Splitting longitude and latitude...');
 crime$long <- NA;
 crime$lat <- NA;
 for (i in seq_along(crime$Location)) {
@@ -21,8 +25,10 @@ for (i in seq_along(crime$Location)) {
   crime$long[i] <- as.numeric(substr(strsplit(as.character(crime$Location[i]),",")[[1]][2],2,
     nchar(strsplit(as.character(crime$Location[i]),",")[[1]][2])-1));
 }
+rm(i);
 
 # Obtain zipcodes from long/lat (estimation by closeness to zip centroids)
+print('Mapping zipcodes by proximity...');
 zipcodes <- read.csv('../data/zipcode.csv',header=TRUE);
 zipcodes <- zipcodes[zipcodes$state == 'MA',c(4,5,1)];
 
@@ -35,11 +41,12 @@ getClosestZip <- function(lat,long) {
     return(temp[which.min(temp$dif),3]);
 }
 
-print('Calculating zipcodes...');
 crime$zip <- NA;
 for (i in 1:dim(crime)[1]) {
     crime$zip[i] <- getClosestZip(crime$lat[i],crime$long[i]);
 }
+rm(i);
+
 # Make sure we have nice 5-digit strings as zipcodes
 crime$zip <- sprintf('%05.0f',as.numeric(as.character(crime$zip)));
 crime$zip[crime$zip=='000NA'] <- NA;
@@ -47,6 +54,12 @@ crime$zip[crime$zip=='000NA'] <- NA;
 # round DOWN dates to 15-minute intervals
 crime$FROMDATE <- as.POSIXct(format(strptime("1969-12-31 19:00", "%Y-%m-%d %H:%M") +
         round(as.numeric(crime$FROMDATE)/900)*900,"%Y-%m-%d %H:%M:%S"));
+
+# Delete unneeded fields
+crime <- data.frame(crime$INCIDENT_TYPE_DESCRIPTION,crime$FROMDATE,crime$WEAPONTYPE,
+    crime$Shooting,crime$DOMESTIC,crime$long,crime$lat,crime$zip);
+names(crime) <- c('type','datetime','weaponType','shooting','domestic','long','lat','zip');
+
 
 # Write out CSV file
 print('Writing CSV file...');
@@ -56,4 +69,6 @@ write.csv(crime,'crime.csv',row.names=FALSE);
 print('Saving R objects...');
 save(crime,file='crime.R');
 
+# Remove temp variables
+rm(getClosestZip,zipcodes);
 print('Done!');
