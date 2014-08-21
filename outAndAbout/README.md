6f05798b7c1ef2be7c7006f712da7dca4976c8c3
David Lago <davidlago@gmail.com>
2014-08-21 16:12:26 -0400

    Grouped transactions in a different way, better suited for analysis.

--- outAndAbout/README.md
+++ outAndAbout/README.md
@@ -2,18 +2,14 @@
 The purpose of this script is producing datasets containing number of events by zipcode in order to explore possible correlations between them.
 
 ## Transactions by zipcode (`txs.csv`)
-This dataset contains the number of transactions per zipcode per 15-minute interval. We can use this data as a proxy for how many people were in a specific zipcode area at a given time. We will aggregate data from T station transactions and cab pick-ups and drop-offs. For simplicity, we will equate one cab transaction to one person, although we might introduce weights if we need to (maybe 1.5 people in average per ride).
-
-There are four 15-minute windows: `00`, `15`, `30` and `45`. Since we are using the number of transactions as a proxy for the number of people at a speficic time, the following adjustments were made to the datasets:
-
-* Train ridership: since these data are already divided in 15-minute blocks, their dates are unchanged.
-* Bus ridership: no data is used (missing zipcodes or location of bus stops).
-* Cab pick-ups and drop-offs: we will round them to the nearest 15-minunte interval.
+This dataset contains the number of transactions per zipcode day. We can use this data as a proxy for how many people were in a specific zipcode area in a specific night. We will aggregate data from T station transactions and cab pick-ups and drop-offs. For simplicity, we will equate one cab transaction to one person, although we might introduce weights if we need to (maybe 1.5 people in average per ride).
 
 ####Field descriptions:
 
 * `zip`: 5 digit string representation of the zipcode.
-* `datetime`: date and time of the start of the 15-minute window. Format: `yyyy-mm-dd HH:MM:ss`.
-* `tx`: number of transactions.
+* `year`: year.
+* `nday`: day number in the year, only counting Fridays and Saturdays (i.e.: the first Friday of the year is day #1, the following Saturday #2, the following week's Friday #3 and so on).
+* `tx`: number of transactions. Includes transactions from 10 pm until 4 am of the following day.
 
 ## Crimes by zipcode (`crime.csv`)
+This is a dataset that 
--- outAndAbout/getTransactionsByZip.R
+++ outAndAbout/getTransactionsByZip.R
@@ -1,3 +1,8 @@
+################################################################
+# This script aggregates the transactions from T and cab by    #
+# 1-hour timeslots and zipcode (Friday and Saturday nights)    #
+################################################################
+
 # Loads datasets
 load('../import/lateNightT.R');
 
@@ -6,34 +11,64 @@ library(plyr);
 print('Loading data...');
 txCabStart <- data.frame(cab$startZip, cab$startDate, 1);
 names(txCabStart) <- c('zip','datetime','tx');
-txCabStart <- txCabStart[!is.na(txCabStart$zip),];
-# round DOWN dates
-txCabStart$datetime <- as.POSIXct(format(strptime("1969-12-31 19:00", "%Y-%m-%d %H:%M") +
-        round(as.numeric(txCabStart$datetime)/900)*900,"%Y-%m-%d %H:%M:%S"));
-
+txCabStart <- txCabStart[!is.na(txCabStart$zip) & !is.na(txCabStart$datetime),];
 txCabEnd <- data.frame(cab$endZip, cab$endDate, 1);
 names(txCabEnd) <- c('zip','datetime','tx');
-txCabEnd <- txCabEnd[!is.na(txCabEnd$zip),];
-# round UP dates
-txCabEnd$datetime <- as.POSIXct(format(strptime("1969-12-31 19:00", "%Y-%m-%d %H:%M") +
-        round(as.numeric(txCabEnd$datetime)/900)*900,"%Y-%m-%d %H:%M:%S"));
-
-# group train transactions by zipcode and date
-print('Grouping trains...');
-txTrain <- ddply(ridership[!is.na(ridership$zip),], c('zip','datetime'), function(df)sum(df$tx));
+txCabEnd <- txCabEnd[!is.na(txCabEnd$zip) & !is.na(txCabEnd$datetime),];
+txTrain <- data.frame(ridership$zip, ridership$datetime, ridership$tx);
 names(txTrain) <- c('zip','datetime','tx');
+txTrain <- txTrain[!is.na(txTrain$zip) & !is.na(txTrain$datetime),];
 
 # join datasets
+print('Joining datasets...');
 txs <- rbind(txTrain,txCabStart,txCabEnd);
 names(txs) <- c('zip','datetime','tx');
 
-# group again, now with all the data
-print('Final grouping...');
-txs <- ddply(txs, c('zip','datetime'), function(df)sum(df$tx));
-names(txs) <- c('zip','datetime','tx');
+# Convert dates to POSIXlt
+txs$datetime <- as.POSIXlt(txs$datetime);
+
+# Filter out txs not in weekend nights and out of range (3/1/2013-6/1/2014)
+print('Adjusting dates and filtering...');
+txs <- txs[txs$datetime >= as.POSIXct('2013-03-01','%Y-%m-%d') &
+            txs$datetime <= as.POSIXct('2014-06-02','%Y-%m-%d'),];
+txs$hour <- txs$datetime[['hour']];
+txs <- txs[txs$hour %in% c(22,23,0,1,2,3),];
+
+
+# Bring records after midnight to previous day
+txs[txs$hour %in% c(0,1,2,3),'datetime'] <- txs[txs$hour %in% c(0,1,2,3),'datetime'] - (24*60*60);
+txs$wday <- txs$datetime[['wday']];
+txs <- txs[txs$wday %in% c(5,6),];
+txs$zip <- as.factor(txs$zip);
+txs$wday <- as.factor(txs$wday);
+levels(txs$wday) <- c('fri','sat');
+txs$hour[txs$hour %in% c(0,1,2,3)] <- txs$hour[txs$hour %in% c(0,1,2,3)] + 24;
+txs$year <- txs$datetime[['year']];
+txs$year <- as.factor(txs$year);
+levels(txs$year) <- c('2013', '2014');
+
+
+# Calculate day number
+txs$nday <- NA;
+txs$nday[txs$wday == 'fri' & txs$year == '2013'] <- as.integer(((txs$datetime[
+    txs$wday == 'fri' & txs$year == '2013'][['yday']]-3)*2/7)+1);
+txs$nday[txs$wday == 'sat' & txs$year == '2013'] <- as.integer(((txs$datetime[
+    txs$wday == 'sat' & txs$year == '2013'][['yday']]-3)*2/7)+2);
+txs$nday[txs$wday == 'fri' & txs$year == '2014'] <- as.integer(((txs$datetime[
+    txs$wday == 'fri' & txs$year == '2014'][['yday']]-2)*2/7)+1);
+txs$nday[txs$wday == 'sat' & txs$year == '2014'] <- as.integer(((txs$datetime[
+    txs$wday == 'sat' & txs$year == '2014'][['yday']]-2)*2/7)+2);
+
+# group transactions
+print('Grouping...');
+txs <- data.frame(txs$zip, txs$year, txs$nday, txs$wday, txs$hour, txs$tx);
+names(txs) <- c('zip','year','nday','wday','hour','tx');
+txs <- ddply(txs, c('zip','year','nday'), function(df)sum(df$tx));
+names(txs) <- c('zip','year','nday','tx');
+
 
 # Delete unneeded data frames
-rm(txCabStart,txCabEnd,txTrain);
+rm(txCabStart,txCabEnd,txTrain,cab,licenses,ridership);
 
 # Write out CSV file
 print('Writing CSV file...');
