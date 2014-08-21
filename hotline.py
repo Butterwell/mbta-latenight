e6f21f5bfe89811685cef8cd530cbb0b52a9b73f
AlgorithmicAscension <kenshin@Sakabato.(none)>
2014-08-21 00:10:21 -0400

    The hotline information has been shortened to
    datetime opened,
    zip code,
    longitude,
    latitude,
    case#
    
    The datetime now conforms to the formats in existing csv (change
    to 2400 and dashes for date rather than slashes).
    
    I am unsure of the convention used for placing these files into
    folders, so I will change relative paths after someone else does
    this.
    
    For now, 'python hotline.py' will open 'Mayor_s_24_Hour_Hotline__
    Service_Requests.csv', find cases which had been opened after
    2014-03-01, between 10pm-3am, find the missing zipcode, format
    the datetime, and write the above columns into 'hotline.csv'

---
+++ hotline.py
@@ -0,0 +1,49 @@
+import datetime as dt
+import csv
+import numpy as np
+
+# utility for longitude, latitude in Massachusetts
+zip_file_object = csv.reader(open('zipcode.csv', 'rb'))
+header = zip_file_object.next()
+data_zip = []
+for row in zip_file_object:
+	data_zip.append(row)
+data_zip = np.array(data_zip)
+MA = np.array([row for row in data_zip if row[2]=='MA'])
+
+#minimum Eucliean distance to centroid of land mass of zip code
+def ll2zip(longitude, latitude):
+	distance = [(float(MA[i,4])-longitude)**2 + (float(MA[i,4])-latitude)**2 for i in range(MA.shape[0])]
+	return MA[distance.index(min(distance)),0]
+
+
+
+csv_file_object = csv.reader(open('data/Mayor_s_24_Hour_Hotline__Service_Requests.csv', 'rb'))
+header = csv_file_object.next()
+data = []
+for row in csv_file_object:
+	dt_opened = dt.datetime.strptime(row[1], "%m/%d/%Y %I:%M:%S %p")
+	wd = dt_opened.weekday()
+	hour = dt_opened.hour
+	##Fridays, Saturdays, and Sundays, after 10pm or before 3am
+	#if (dt_opened > dt.datetime(2013,03,01)) and (wd in [4,5,6]) and (hour>=22 | hour <=3):
+	#	#but not Friday before 3am or Sundays after 10pm
+	#	if (not (wd==4 & hour<=3)) or (not (wd==6 & hour>=22)):
+	#		data.append(row)
+	if (dt_opened > dt.datetime(2013,03,01)):
+		if (wd==4 and hour >=22) or (wd==5 and hour < 3) or (wd==5 and hour >=22) or (wd==6 and hour < 3):
+			data.append(row)
+data = np.array(data)
+
+
+for row in data:
+	row[1] = dt.datetime.strptime(row[1], "%m/%d/%Y %I:%M:%S %p").strftime("%m-%d-%Y %H:%M:%S")
+	if row[22] == "":
+		row[22] = ll2zip(float(row[26]),float(row[25]))
+#re-arrange columns into: (note, tab width: 4)
+						#datetime opened,	zipcode,	longitude,	latitude,	case# - check original csv for details
+data = np.column_stack(	(data[:,1], 		data[:,22],	data[:,26],	data[:,25],	data[:,0])	)
+
+open_file_object = csv.writer(open("hotline.csv", "wb"))
+for row in data:
+	open_file_object.writerow(row)
