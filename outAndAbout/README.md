14ac3ebfd1218820283073e8fe5ec5a94b507ead
David Lago <davidlago@gmail.com>
2014-08-21 16:36:50 -0400

    Added new crime dataset in same format as transactions.csv

--- outAndAbout/README.md
+++ outAndAbout/README.md
@@ -2,7 +2,7 @@
 The purpose of this script is producing datasets containing number of events by zipcode in order to explore possible correlations between them.
 
 ## Transactions by zipcode (`txs.csv`)
-This dataset contains the number of transactions per zipcode day. We can use this data as a proxy for how many people were in a specific zipcode area in a specific night. We will aggregate data from T station transactions and cab pick-ups and drop-offs. For simplicity, we will equate one cab transaction to one person, although we might introduce weights if we need to (maybe 1.5 people in average per ride).
+This dataset contains the number of transactions per zipcode per day. We can use this data as a proxy for how many people were in a specific zipcode area in a specific night. We will aggregate data from T station transactions and cab pick-ups and drop-offs. For simplicity, we will equate one cab transaction to one person, although we might introduce weights if we need to (maybe 1.5 people in average per ride).
 
 ####Field descriptions:
 
@@ -12,4 +12,11 @@ This dataset contains the number of transactions per zipcode day. We can use thi
 * `tx`: number of transactions. Includes transactions from 10 pm until 4 am of the following day.
 
 ## Crimes by zipcode (`crime.csv`)
-This is a dataset that 
+This dataset contains the number of crime events reported per zipcode per day.
+
+####Field descriptions:
+
+* `zip`: 5 digit string representation of the zipcode.
+* `year`: year.
+* `nday`: day number in the year, only counting Fridays and Saturdays (i.e.: the first Friday of the year is day #1, the following Saturday #2, the following week's Friday #3 and so on).
+* `events`: number of reported events. Includes events from 10 pm until 4 am of the following day.
--- outAndAbout/getCrimeByZip.R
+++ outAndAbout/getCrimeByZip.R
@@ -9,12 +9,13 @@ unzip('../data/crime.zip', exdir='../data')
 crime <- read.csv('../data/Crime_Incident_Reports.csv', header=TRUE);
 
 # Convert date
-crime$FROMDATE <- as.POSIXct(strptime(crime$FROMDATE,"%m/%d/%Y %I:%M:%S %p"));
-# Filter out entries prior to 2013-03-01
-crime <- crime[crime$FROMDATE >= as.POSIXct(strptime("2013-03-01", "%Y-%m-%d")),];
+crime$FROMDATE <- as.POSIXlt(strptime(crime$FROMDATE,"%m/%d/%Y %I:%M:%S %p"));
+# Filter out entries prior to 2013-03-01 or after 2014-06-1 and not on weekend nights
+crime <- crime[crime$FROMDATE >= as.POSIXct(strptime("2013-03-01", "%Y-%m-%d")) &
+            crime$FROMDATE <= as.POSIXct('2014-06-02','%Y-%m-%d') ,];
 crime <- crime[!is.na(crime$COMPNOS),];
-# Limit to just weekend
-crime <- crime[crime$DAY_WEEK == 'Friday' | crime$DAY_WEEK == 'Saturday' | crime$DAY_WEEK == 'Sunday',];
+crime$hour <- crime$FROMDATE[['hour']];
+crime <- crime[crime$hour %in% c(22,23,0,1,2,3),];
 
 # Split longitude and latitude
 print('Splitting longitude and latitude...');
@@ -51,15 +52,45 @@ rm(i);
 crime$zip <- sprintf('%05.0f',as.numeric(as.character(crime$zip)));
 crime$zip[crime$zip=='000NA'] <- NA;
 
-# round DOWN dates to 15-minute intervals
-crime$FROMDATE <- as.POSIXct(format(strptime("1969-12-31 19:00", "%Y-%m-%d %H:%M") +
-        round(as.numeric(crime$FROMDATE)/900)*900,"%Y-%m-%d %H:%M:%S"));
 
 # Delete unneeded fields
-crime <- data.frame(crime$INCIDENT_TYPE_DESCRIPTION,crime$FROMDATE,crime$WEAPONTYPE,
+crime <- data.frame(crime$INCIDENT_TYPE_DESCRIPTION,crime$FROMDATE,crime$hour,crime$WEAPONTYPE,
     crime$Shooting,crime$DOMESTIC,crime$long,crime$lat,crime$zip);
-names(crime) <- c('type','datetime','weaponType','shooting','domestic','long','lat','zip');
+names(crime) <- c('type','datetime','hour','weaponType','shooting','domestic','long','lat','zip');
+crime$datetime <- as.POSIXlt(crime$datetime);
 
+# Bring records after midnight to previous day
+crime[crime$hour %in% c(0,1,2,3),'datetime'] <- crime[crime$hour %in% c(0,1,2,3),'datetime'] - (24*60*60);
+crime$wday <- crime$datetime[['wday']];
+crime <- crime[crime$wday %in% c(5,6),];
+crime$wday <- as.factor(crime$wday);
+levels(crime$wday) <- c('fri','sat');
+crime$hour[crime$hour %in% c(0,1,2,3)] <- crime$hour[crime$hour %in% c(0,1,2,3)] + 24;
+crime$year <- crime$datetime[['year']];
+crime$year <- as.factor(crime$year);
+levels(crime$year) <- c('2013', '2014');
+
+# Calculate day number
+crime$nday <- NA;
+crime$nday[crime$wday == 'fri' & crime$year == '2013'] <- as.integer(((crime$datetime[
+    crime$wday == 'fri' & crime$year == '2013'][['yday']]-3)*2/7)+1);
+crime$nday[crime$wday == 'sat' & crime$year == '2013'] <- as.integer(((crime$datetime[
+    crime$wday == 'sat' & crime$year == '2013'][['yday']]-3)*2/7)+2);
+crime$nday[crime$wday == 'fri' & crime$year == '2014'] <- as.integer(((crime$datetime[
+    crime$wday == 'fri' & crime$year == '2014'][['yday']]-2)*2/7)+1);
+crime$nday[crime$wday == 'sat' & crime$year == '2014'] <- as.integer(((crime$datetime[
+    crime$wday == 'sat' & crime$year == '2014'][['yday']]-2)*2/7)+2);
+
+# group transactions
+print('Grouping...');
+crime <- data.frame(crime$zip, crime$year, crime$nday, crime$wday, crime$hour);
+names(crime) <- c('zip','year','nday','wday','hour');
+crime$events <- 1;
+crime <- ddply(crime, c('zip','year','nday'), function(df)sum(df$events));
+names(crime) <- c('zip','year','nday','events');
+
+# Delete unneeded data frames
+rm(getClosestZip,zipcodes);
 
 # Write out CSV file
 print('Writing CSV file...');
