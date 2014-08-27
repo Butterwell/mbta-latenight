e13b015f261a89f26f06010649a07bbf8f211462
David Lago <davidlago@gmail.com>
2014-08-26 20:52:22 -0400

    Added placeholder for complaints data

--- outAndAbout/analyze.R
+++ outAndAbout/analyze.R
@@ -7,7 +7,13 @@ ems <- read.csv('../import/EMS.csv',header=T);
 merged <- merge(txs,crime, by=c('zip','year','nday'),all.x=T);
 merged <- merge(merged,ems,by=c('year','nday'),all.x=T);
 
-names(merged) <- c('year','nday','zip','people','crime','emergencies');
+# PLACEHOLDER FOR MAYOR'S HOTLINE DATA###
+merged$complaints <- NA;
+#########################################
+
+names(merged) <- c('year','nday','zip','people','crime','emergencies','complaints');
+
+
 
 # Write out CSV file
 print('Writing CSV file...');
