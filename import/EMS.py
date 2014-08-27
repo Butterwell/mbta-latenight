4a9e3435a4678211c2c4410c2ceefce176bb5074
AlgorithmicAscension <kenshin@Sakabato.(none)>
2014-08-26 20:07:31 -0400

    The hotline information has been shortened to
    datetime opened,
    zip code,
    longitude,
    latitude,
    case#
    
    The datetime now conforms to the formats in existing csv (change
    to 2400 and dashes for date rather than slashes).
    
    For now, 'python hotline.py' will open 'Mayor_s_24_Hour_Hotline__
    Service_Requests.csv', find cases which had been opened after
    2014-03-01, between 10pm-3am, find the missing zipcode, format
    the datetime, and write the above columns into 'hotline.csv'
    
    Boston Emergency Medical Response (EMS) data was added. It has 3 years
    of incidents from the 5 hour interval Fridays 10pm - Saturdays 3am.
    
    data/EMS.csv, import/EMS.py, import/EMS.csv was added.

---
+++ import/EMS.py
@@ -0,0 +1,21 @@
+import datetime as dt
+import csv
+import numpy as np
+
+csv_file_object = csv.reader(open('../data/EMS.csv', 'rb'))
+header = csv_file_object.next()
+data = []
+for row in csv_file_object:
+	data.append(row)
+
+data = np.array(data)
+
+for row in data:
+	row[0] = dt.datetime.strptime(row[0].split('-')[0], "%m/%d/%y").strftime("%Y %W %w")
+
+data = np.c_[ [x.split(' ')[0] for x in data[:,0]], [str(2*int(x.split(' ')[1])) for x in data[:,0]], data[:,1]]
+
+open_file_object = csv.writer(open("EMS.csv", "wb"))
+open_file_object.writerow(['year', 'nday', 'emergencyResponses'])
+for row in data:
+	open_file_object.writerow(row)
